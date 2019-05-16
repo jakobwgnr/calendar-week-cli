c7519f40a471154ae9acc3f1e4a290c939f80552
Wagner <jaykob.wagner@gmail.com>
2019-05-16 11:37:39 +0200

    minor enhancements

--- index.js
+++ index.js
@@ -10,30 +10,39 @@ const program = require('commander');
 const chalk = require('chalk');
 const pjson = require('./package.json');
 
-program
-    .version(pjson.version)
-    .option('-d, --date <date>', 'provide a specific Date')
-    .action(function() {
-        var parseDate = new Date();
-        if(program.date) {
-            parseDate = new Date(program.date);
-        }
-        result = getWeekNumber(parseDate);
-        console.log('It\'s currently week ' + chalk.bold.cyan(result[1]) + ' of ' + chalk.bold.cyan(result[0]));
-    })
-    .parse(process.argv);
-
+// *******************************
+// Helper Functions
+// *******************************
 
 function getWeekNumber(d) {
-    // Copy date so don't modify original
-    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
-    // Get first day of year
-    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-    // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
-    // Return array of year and week number
-    return [d.getUTCFullYear(), weekNo];
+  // Copy date so don't modify original
+  const date = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+
+  // Set to nearest Thursday: current date + 4 - current day number
+  // Make Sunday's day number 7
+  date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+
+  // Get first day of year
+  const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+
+  // Calculate full weeks to nearest Thursday
+  const weekNo = Math.ceil(((date - yearStart) / 86400000 + 1) / 7);
+
+  // Return array of year and week number
+  return [date.getUTCFullYear(), weekNo];
 }
+
+// **********************
+// Public Interface
+// **********************
+
+program
+  .version(pjson.version)
+  .option('-d, --date <date>', 'provide a specific Date')
+  .action(() => {
+    const result = getWeekNumber(program.date ? new Date(program.date) : new Date());
+    console.log(`It's calendar week ${chalk.bold.cyan(result[1])} in ${chalk.bold.cyan(result[0])}`);
+  })
+  .parse(process.argv);
+
+export default { getWeekNumber };
